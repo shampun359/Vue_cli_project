f1843d24892f35c8a92e34b367fe257296a4c76d
Mikhail <begasos1997@gmail.com>
2022-02-19 18:39:25 +0400

    Попытки   провалились

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,4 +1,8 @@
 <template>
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     <ul class="catalog__list">  <!--Список товаров-->
         <li class="catalog__item" v-for="(product, index) in products" :key="index">  <!--товар-->
             <a class="catalog__pic" href="#">
@@ -15,6 +19,24 @@
               {{ product.price }} ₽
             </span>
 
+<<<<<<< Updated upstream
+=======
+      <li class="catalog__item" v-for="(product, index) in products" :key="index">  <!--товар-->
+            <a class="catalog__pic" href="#">
+              <img :src="product.image"  alt="{{ product.title }}">
+            </a>
+
+            <h3 class="catalog__title">
+              <a href="#">
+                {{ product.title }}
+              </a>
+            </h3>
+
+            <span class="catalog__price">
+              {{ product.price }} ₽
+            </span>
+
+>>>>>>> Stashed changes
             <ul class="colors colors--black">
               <li class="colors__item">
                 <label class="colors__label" for='gg'>
@@ -41,7 +63,12 @@
                 </label>
               </li>
             </ul>
+<<<<<<< Updated upstream
           </li>
+=======
+      </li>
+
+>>>>>>> Stashed changes
     </ul>
 
 </template>
