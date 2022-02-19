207dcd965c45960f2d7254c51ebe70c179cb8deb
Mikhail <begasos1997@gmail.com>
2022-02-19 13:52:20 +0400

    Сделал  первый массив

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,111 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <section class="catalog">  <!--Секция каталог товаров-->
+        <ul class="catalog__list">  <!--Список товаров-->
+
+          <li class="catalog__item" v-for="(product, index) in products" :key="index">  <!--товар-->
+            <a class="catalog__pic" href="#">
+              <img :src="product.image"  alt="{{ product.title }}">
+            </a>
+
+            <h3 class="catalog__title">
+              <a href="#">
+                {{ product.title }}
+              </a>
+            </h3>
+
+            <span class="catalog__price">
+              {{ product.price }} ₽
+            </span>
+
+            <ul class="colors colors--black">
+              <li class="colors__item">
+                <label class="colors__label" for='gg'>
+                  <input class="colors__radio sr-only" type="radio" name="color-1"
+                    value="#73B6EA" checked="" id="gg">
+                  <span class="colors__value" style="background-color: #73B6EA;">
+                  </span>
+                </label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label"  for="asd">
+                  <input class="colors__radio sr-only" type="radio" name="color-1"
+                    value="#8BE000" id="asd">
+                  <span class="colors__value" style="background-color: #8BE000;">
+                  </span>
+                </label>
+              </li>
+              <li class="colors__item">
+                <label class="colors__label"  for="qwerty">
+                  <input class="colors__radio sr-only" type="radio" name="color-1"
+                    value="#222" id="qwerty">
+                  <span class="colors__value" style="background-color: #222;">
+                  </span>
+                </label>
+              </li>
+            </ul>
+          </li>
+        </ul>
+
+        <ul class="catalog__pagination pagination"><!--Слайдер ...  На   след. страницу-->
+          <li class="pagination__item">
+            <a class="pagination__link pagination__link--arrow pagination__link--disabled"
+            aria-label="Предыдущая страница">
+              <svg width="8" height="14" fill="currentColor">
+                <use xlink:href="#icon-arrow-left"></use>
+              </svg>
+            </a>
+          </li>
+          <li class="pagination__item">
+            <a class="pagination__link pagination__link--current">
+              1
+            </a>
+          </li>
+          <li class="pagination__item">
+            <a class="pagination__link" href="#">
+              2
+            </a>
+          </li>
+          <li class="pagination__item">
+            <a class="pagination__link" href="#">
+              3
+            </a>
+          </li>
+          <li class="pagination__item">
+            <a class="pagination__link" href="#">
+              4
+            </a>
+          </li>
+          <li class="pagination__item">
+            <a class="pagination__link" href="#">
+              ...
+            </a>
+          </li>
+          <li class="pagination__item">
+            <a class="pagination__link" href="#">
+              10
+            </a>
+          </li>
+          <li class="pagination__item">
+            <a class="pagination__link pagination__link--arrow" href="#"
+            aria-label="Следующая страница">
+              <svg width="8" height="14" fill="currentColor">
+                <use xlink:href="#icon-arrow-right"></use>
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import products from './data/products'; /* Импортируем массив с товарами */
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      products,
+    };
   },
 };
 </script>
-
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
---
+++ src/data/products.js
@@ -0,0 +1,64 @@
+/*  Массив  с  объектами товаров, экспортируется в vue.app */
+export default [
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: 'img/radio.jpg',
+  },
+  {
+    title: 'Redmi not 8 PRO',
+    price: 12000,
+    image: 'img/redmi8.jpg',
+  },
+  {
+    title: 'Redmi not 9 PRO',
+    price: 17000,
+    image: 'img/redmi8.jpg',
+  },
+  {
+    title: 'Redmi not 10 PRO',
+    price: 27000,
+    image: 'img/redmi8.jpg',
+  },
+  {
+    title: 'Iphone',
+    price: 50000,
+    image: 'img/redmi8.jpg',
+  },
+  {
+    title: 'Зубная щётка электрическая',
+    price: 1500,
+    image: 'img/oralb-el.jpg',
+  },
+  {
+    title: 'Зубная щётка электрическая',
+    price: 5500,
+    image: 'img/oralb-el.jpg',
+  },
+  {
+    title: 'Гироскутер',
+    price: 25000,
+    image: 'img/ripstik.jpg',
+  },
+  {
+    title: 'Скейт',
+    price: 4000,
+    image: 'img/board.jpg',
+  },
+  {
+    title: 'Велс-крутой',
+    price: 40000,
+    image: 'img/wheels.jpg',
+  },
+  {
+    title: 'Велик',
+    price: 18000,
+    image: 'img/bicycle.jpg',
+  },
+  {
+    title: 'Велик клон',
+    price: 20000,
+    image: 'img/bicycle.jpg',
+  },
+
+];
